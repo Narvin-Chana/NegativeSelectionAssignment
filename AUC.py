cbb2f4922c09dfbe8cd8f2868b750c15ad8e2593
Narvin Chana <36044215+jumsee@users.noreply.github.com>
2022-04-01 19:06:51 +0200

    Update AUC.py

--- AUC.py
+++ AUC.py
@@ -1,11 +1,11 @@
 import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
-from itertools import cycle
+
 from sklearn.metrics import roc_curve, auc
 
 n = 10
-r = 4
+r = 3
 
 files = {
     "tagalog.test",
@@ -15,20 +15,25 @@ files = {
     "lang/xhosa.txt"
 }
 
-subprocess.run(f'java -jar negsel2.jar -self english.train -n {n} -r {r} -c -l < ./english.test > res.txt', shell=True)
+subprocess.run(f'java -jar negsel2.jar -self english.train -n {n} -r {r} -c -l < ./english.test > res.txt',
+               shell=True)
 englishData = np.loadtxt('res.txt')
 
 for test_set in files:
 
-    subprocess.run(f'java -jar negsel2.jar -self english.train -n {n} -r {r} -c -l < {test_set} > ./{test_set}result', shell=True)
+    subprocess.run(f'java -jar negsel2.jar -self english.train -n {n} -r {r} -c -l < {test_set} > ./{test_set}result',
+                   shell=True)
 
     test_data = np.loadtxt(f'{test_set}result')
 
     y_score = np.concatenate((englishData, test_data))
-    y_true = np.array(([1 for _ in range(len(englishData))] + [0 for _ in range(len(test_data))]))
+    y_true = np.concatenate((np.zeros(englishData.size, dtype=np.int32), np.ones(test_data.size, dtype=np.int32)))
+    y_sorted = np.argsort(y_score)
+    y_score = y_score[y_sorted]
+    y_true = y_true[y_sorted]
 
     # Compute ROC curve and ROC area
-    fpr, tpr, _ = roc_curve(y_true, y_score, pos_label=0)
+    fpr, tpr, _ = roc_curve(y_true, y_score, pos_label=1)
     roc_auc = auc(fpr, tpr)
 
     plt.figure()
